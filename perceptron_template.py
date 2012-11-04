f8786d41a1313653df10329f03356f0faacf7258
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-11-03 22:15:33 -0400

    Bug fixes for perceptron_template.py

--- perceptron_template.py
+++ perceptron_template.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 import sys
 import random
-# use random to randomly initialize weights and sample data randomly
-
 
 def configure():
     """Reads configuration data from the file.
@@ -28,66 +26,55 @@ def configure():
 
 
 def read_data(infile, input_count):
-    """Reads the data from the file into a list of inputs and outputs
-    """
+    """Reads the data from the file into a list of inputs and outputs."""
     example_inputs = []
     example_outputs = []
 
     for line in infile.readlines():
         line = line.split()
-        example_inputs.append([int(x) for x in line[:input_count]])
+        # note added bias term, a fixed input of 1.0
+        example_inputs.append([int(x) for x in line[:input_count]] + [1.0])
         example_outputs.append([int(x) for x in line[input_count:]])
     return example_inputs, example_outputs
 
 
 def print_weights(network, input_count, output_count):
-    """Prints the weight matrix of a single layer perceptron.
-    """
+    """Prints the weight matrix of a single layer perceptron."""
     for neuron_index in xrange(input_count + 1):
         for output_index in xrange(output_count):
             print "%s   " % network[neuron_index][output_index],
-        # prints a newline
-        print ""
-
-
-def validate(network, train_count, example_count, output_count,
-             example_outputs):
-    """Returns the number of correctly classified samples from the cross
-    validation set.
-    """
-    correct = 0
-    for i in xrange(train_count, example_count):
-        # compute prediction, it will be list with one of these zeroes as a one
-        pred_outputs = [0 for x in xrange(output_count)]
-        if all(map(lambda x, y: x == y, pred_outputs, example_outputs[i])):
-            correct += 1
-    return correct
-
+        print
 
 def main():
-    """Example main function.
-    """
     infile, example_count, input_count, output_count = configure()
     example_inputs, example_outputs = read_data(infile, input_count)
 
     # take the first 90% of the examples
     train_count = int(0.9 * example_count)
 
+    alpha = 0.01
+
+    pred_outputs = [0 for _ in xrange(output_count)]
+
     # network is a matrix of weights; rows represent input values + 1
     # bias term, columns represent output nodes; so reading down the
     # matrix, for some column, gives the weights for the perceptron
     # associated with the column's output node
 
-    # ..........
+    .............
 
     # uncomment this to print the network to the terminal
     # print_weights(network, input_count, output_count)
 
     # try some predictions
-    correct = validate(network, train_count, example_count, output_count,
-                       example_outputs)
-    print "Correct: %f" % (float(correct) / (example_count - train_count))
+    correct = 0
+    for e in xrange(train_count, example_count):
 
+        .............
+
+        if all(map(lambda x, y: x == y, pred_outputs, example_outputs[e])):
+            correct += 1
+    print "Correct: %f" % (float(correct) / (example_count - train_count))
 
 if __name__ == "__main__":
     main()
